65a230620a972fdb581b54e09aee280b60d4b645
Ben Titcomb <btitcomb@scpr.org>
2018-04-25 19:01:11 -0700

    Generalize the demodulator and make it easier to add other data like strings to the output.

--- test/test.js
+++ test/test.js
@@ -8,38 +8,32 @@ const assert              = require('chai').assert,
     { createReadStream }  = require('fs'),
     { Readable,
       Writable }          = require('stream'),
-      Goertzel            = require('goertzeljs'),
-      Speaker             = require('audio-speaker/stream'),
-      FREQUENCY   = 18000,
-      DEVIATION   = 100,
-      SAMPLE_RATE = 44100,
-      SAMPLES_PER = 50,
-      EASE        = ((0.00225 * SAMPLES_PER) / SAMPLE_RATE),
-      PREAMBLE    = Uint8Array.from([ 170, 170, 170 ]).buffer;
+      FREQUENCY           = 18000,
+      DEVIATION           = 100,
+      SAMPLE_RATE         = 44100,
+      SAMPLES_PER         = 35,
+      WINDOW              = SAMPLES_PER,
+      STEP                = Math.round(WINDOW * 0.33),
+      EASE                = ((0.00225 * SAMPLES_PER) / SAMPLE_RATE),
+      PREAMBLE            = Uint8Array.from([ 170, 170, 170 ]).buffer;
 
 class BensDemodulator extends Demodulator {
   constructor(options={}){
-    super(...arguments);
-    const { frequency, deviation } = options;
-    this.markFreq  = frequency + deviation;
-    this.spaceFreq = frequency - deviation;
-    this.carrier   = frequency;
-    this.goertzel  = new Goertzel({
-      frequencies:  [
-        this.markFreq,
-        this.spaceFreq,
-        this.carrier
-      ],
-      sampleRate: options.sampleRate || 44100
-    });
-    this.previous = -1;
+    const { frequency, deviation } = options,
+          markFreq  = frequency + deviation,
+          spaceFreq = frequency - deviation,
+          carrier   = frequency;
+    options.frequencies = [markFreq, spaceFreq, carrier];
+    super(options);
+    this.markFreq       = markFreq;
+    this.spaceFreq      = spaceFreq;
+    this.carrier        = carrier;
+    this.frequencyCount = 3;
+    this.previous       = -1;
   }
-  process(samples){
-    const { goertzel, markFreq, spaceFreq, carrier, previous } = this;
-    samples.forEach(s => goertzel.processSample(s));
-    const energies = Object.entries(goertzel.energies).sort((a,b) => a[1] - b[1]),
-          greatest = parseInt(energies[goertzel.frequencies.length - 1][0]);
-    goertzel.refresh();
+  demodulate(frequencyData){
+    const { markFreq, spaceFreq, carrier, previous } = this,
+            greatest = parseInt(frequencyData[this.frequencyCount- 1][0]);
     let value;
     if(greatest == markFreq) {
       value = 1;
@@ -49,7 +43,7 @@ class BensDemodulator extends Demodulator {
       value = -1;
     }
     this.previous = value;
-    if(value > -1 && previous === -1) return value; 
+    if(value > -1 && previous === -1) this.bitStream.writeBits(value, 1);
   }
 }
 
@@ -64,23 +58,23 @@ describe('undertone', function(){
     destination._write = function(chunk, encoding, next){
       const output = String.fromCharCode.apply(null, new Uint8Array(chunk.buffer));
       assert.equal(output, 'hello world');
-      next(), done();
+      done(), next();
     };
     source
       .pipe(encoder({
         preamble: PREAMBLE
       }))
       .pipe(modulator({
-        frequency:     FREQUENCY,
-        deviation:     DEVIATION,
-        samplesPerBit: SAMPLES_PER,
+        frequency:        FREQUENCY,
+        deviation:        DEVIATION,
+        samplesPerSymbol: SAMPLES_PER,
         ease: EASE
       }))
       .pipe(BensDemodulator.createTransformStream({
         frequency:       FREQUENCY,
         deviation:       DEVIATION,
-        windowSize:      SAMPLES_PER,
-        step:            SAMPLES_PER
+        windowSize:      WINDOW,
+        step:            STEP
       }))
       .pipe(decoder({
         preamble: PREAMBLE
